0c31e215b453e7f0b10b91b06d12c041ef4f8773
Kris de Rycke <derycke.kris@gmail.com>
2019-10-01 14:47:44 +0200

    implement contact form (no php)

--- appContact.js
+++ appContact.js
@@ -1,15 +1,8 @@
 window.addEventListener("load", bounce);
 
 function bounce() {
-  let textBox = document.getElementById("php");
+  // let textBox = document.getElementById("php");
+  // let promptBox = document.getElementById("phpBox");
   let form = document.getElementById("form");
-  let promptBox = document.getElementById("phpBox");
-  TweenLite.to(form, 1.5, { ease: Back.easeOut.config(3), x: "200%" });
-
-  if (textBox.innerHTML != "") {
-    console.log(textBox.Text);
-    textBox.style.display = "block";
-    promptBox.style.display = "block";
-    form.style.display = "none  ";
-  }
+  TweenLite.to(form, 1.5, { ease: Back.easeOut.config(3), x: "50%" });
 }
