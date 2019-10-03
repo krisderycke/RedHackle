2247058ddc7013ad4902b3b32ac9325fef39c2da
Kris de Rycke <derycke.kris@gmail.com>
2019-10-03 14:35:26 +0200

    restructer layour folder

--- appContact.js
+++
@@ -1,8 +0,0 @@
-window.addEventListener("load", bounce);
-
-function bounce() {
-  // let textBox = document.getElementById("php");
-  // let promptBox = document.getElementById("phpBox");
-  let form = document.getElementById("form");
-  TweenLite.to(form, 1.5, { ease: Back.easeOut.config(3), x: "50%" });
-}
---
+++ leden.php
@@ -0,0 +1,3 @@
+<?php
+echo '<a href="./assets/pdf/mrs-macleod-of-raasay" target="_blank"> back</a> '
+?>
